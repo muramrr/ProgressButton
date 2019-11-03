d8f1ea0962e1e76c6701060fc08996dc28af3b1f
Andrii Kovalchuk <muram4ik@gmail.com>
2019-11-03 16:09:47 +0200

    added sample project updated gradle dependencies

---
+++ app/src/main/java/com/mmdev/progressbutton/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.mmdev.progressbutton
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.mmdev.progressbuttonlib.ProgressButton
+
+
+/* Created by Andrii on 03.11.2019.*/
+
+/**
+ * This is the documentation block about the class
+ */
+
+class LoginActivity: AppCompatActivity(R.layout.activity_login){
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		val progressButton = findViewById<ProgressButton>(R.id.login)
+		progressButton.setOnClickListener {
+			progressButton.startAnim()
+			Handler().postDelayed({
+				                      progressButton.stopAnim { startMainActivity() }
+			                      }, 2000)
+		}
+	}
+
+	private fun startMainActivity(){
+		val mainActivityIntent = Intent(this, MainActivity::class.java)
+		startActivity(mainActivityIntent)
+		finish()
+	}
+}
---
+++ app/src/main/java/com/mmdev/progressbutton/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mmdev.progressbutton
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity(R.layout.activity_main)
